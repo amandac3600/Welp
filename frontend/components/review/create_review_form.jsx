15ff24d0a71f616962e82d61445cd3472526f888
amandac3600 <amandac3600@gmail.com>
2022-01-26 18:04:34 -0800

    working on create review form

--- frontend/components/review/create_review_form.jsx
+++ frontend/components/review/create_review_form.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NavContainer from '../nav/nav_container'
 
 export default class CreateReviewForm extends React.Component {
   constructor(props) {
@@ -31,7 +32,32 @@ export default class CreateReviewForm extends React.Component {
     if (!this.props.business) return null;
     return (
       <div>
-        <h1 className="create-review-title">{this.props.business.name}</h1>
+        <div className="red-nav">
+          <NavContainer/>
+        </div>
+        <form onSubmit={this.handleSubmit} className="create-review-form">
+          <h1 className="create-review-title">{this.props.business.name}</h1>
+          <div className="create-form-rating-container">
+            <div>
+              <input id="radio5" type="radio" value="5" name="rating" onChange={this.handleChange("rating")} />
+              <label htmlFor="radio5" id="create-form-rating" className="create-form-rating-5"></label>
+
+              <input id="radio4" type="radio" value="4" name="rating" onChange={this.handleChange("rating")} />
+              <label htmlFor="radio4" id="create-form-rating" className="create-form-rating-4"></label>
+              
+              <input id="radio3" type="radio" value="3" name="rating" onChange={this.handleChange("rating")} />
+              <label htmlFor="radio3" id="create-form-rating" className="create-form-rating-3"></label>
+              
+              <input id="radio2" type="radio" value="2" name="rating" onChange={this.handleChange("rating")} />
+              <label htmlFor="radio2" id="create-form-rating" className="create-form-rating-2"></label>
+              
+              <input id="radio1" type="radio" value="1" name="rating" onChange={this.handleChange("rating")} />
+              <label htmlFor="radio1" id="create-form-rating" className="create-form-rating-1"></label>
+            </div>
+            <textarea className="create-review-body"></textarea>
+          </div>
+          <button type="submit" className="create-review-submit">Post Review</button>
+        </form>
       </div>
     )
   }
