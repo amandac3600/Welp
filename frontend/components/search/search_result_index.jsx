6984606bf70c8a21bbe0d27eaa51efb3c628af03
amandac3600 <amandac3600@gmail.com>
2022-02-18 19:24:43 -0800

    update github link

--- frontend/components/search/search_result_index.jsx
+++ frontend/components/search/search_result_index.jsx
@@ -81,11 +81,11 @@ class SearchResultIndex extends React.Component {
         <div className='show-about-discover'>
           <div className='splash-about'>
             <h6 className='splash-about-title'>About</h6>
-            <a className='splash-about-link' href="https://www.linkedin.com/in/amanda-chen-4b175a146">LinkedIn</a>
+            <a className='splash-about-link' href="https://www.linkedin.com/in/amanda-chen-4b175a146" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           </div>
           <div className='splash-discover'>
             <h6 className='splash-discover-title'>Discover</h6>
-            <a className='splash-discover-link' href="https://github.com/amandac3600">Github</a>
+            <a className='splash-discover-link' href="https://github.com/amandac3600/Welp" target="_blank" rel="noopener noreferrer">Github</a>
             <br />
             <div className='splash-discover-link-2'>
               <a className='splash-discover-link' href="mailto:amandac3600@gmail.com?subject=Let's talk about a job!">Email</a>
