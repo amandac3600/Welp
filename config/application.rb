ba1472e9d04e7cf6a99e3193996be3e5128dac3f
amandac3600 <amandac3600@gmail.com>
2023-08-04 00:45:18 -0700

    get app running

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Welp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    
+    require "active_storage/engine"
+    config.active_storage.service = :amazon
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
